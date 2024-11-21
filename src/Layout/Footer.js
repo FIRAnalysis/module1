13960bf5f214d69dcd3a4ea9c6ae2a52ba563089
shruti325 <sharmashruti71930@gmail.com>
2024-11-22 00:40:33 +0530

    second commit

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import '../styles/footer.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/footer.css";
 const Footer = () => {
   return (
-    <div className='footer'>
-      <h4 className='text-center'>All Rights Reserved &copy; Tech Innova Club</h4>
-      <p className='text-center mt-3'>
-      <Link to="/about">About Us</Link>
-      |
-      <Link to="/contact">Contact Us</Link>
-     
-      
-</p>
-      
+    <div className="footer">
+      <h4 className="text-center">Source Code</h4>
+      <p className="text-center mt-3">
+        <Link to="/about">Shreshtha Suri </Link>|
+        <Link to="/contact">Shreya Ameta</Link>|
+        <Link to="/about">Shruti Sharma</Link>|
+        <Link to="/contact">Sonali Jain</Link>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,41 +1,57 @@
-import React from 'react'
+import React from "react";
 
-import {  NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark ">
-  <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <NavLink className="navbar-brand" >TECH INNOVA</NavLink>
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink  to="/home" className="nav-link " aria-current="page" >Home</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/" className="nav-link" href="#">Register</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/login" className="nav-link" href="#">Login</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/event" className="nav-link" href="#">Upcoming Events</NavLink>
-        </li>
-        
-      </ul>
-     
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark ">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <NavLink className="navbar-brand">FIR ANALYSIS</NavLink>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink to="/home" className="nav-link " aria-current="page">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/FAQs" className="nav-link" href="#">
+                  FAQs
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/login" className="nav-link" href="#">
+                  analytics
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/event" className="nav-link" href="#">
+                  Developers
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/event" className="nav-link" href="#">
+                  Register/Login
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
+  );
+};
 
-
-
-    </div>
-  )
-}
-
-export default Header
+export default Header;
